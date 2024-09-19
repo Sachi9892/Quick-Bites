08065de2ca5b4a07f6c06facc7fe946cfbc1962a
Sachi9892 <sachinandtech@gmail.com>
2024-09-19 16:20:35 +0530

    Taking break for exams

--- user-ms/src/main/java/com/quick_bites/controllers/order_controller/order_now/OrderNowController.java
+++ user-ms/src/main/java/com/quick_bites/controllers/order_controller/order_now/OrderNowController.java
@@ -14,6 +14,7 @@ import com.quick_bites.service.managers.order_manager.payment_manager.ICreateRaz
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class OrderNowController {
     private final CartRepository cartRepository;
 
     @PostMapping("/order-now")
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<String> createOrderMethod(@RequestBody PlaceOrderRequestDto orderRequest) {
 
         try {
